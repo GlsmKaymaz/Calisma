4bcc9f882b83e65e69848aab81e27e76ff4ce9ff
Gulsumj <glsmkaymazz@gmail.com>
2024-02-02 03:29:08 +0300

    v1

--- src/IfStatements/forLoop.java
+++ src/IfStatements/forLoop.java
@@ -1,4 +1,14 @@
 package IfStatements;
 
 public class forLoop {
+    public static void main(String[] args) {
+        String[] names = {"John", "Alex", "Dani", "Avi"};
+
+        for (String name : names) {
+            System.out.println("List of names = " + name);
+        }
+
+
+    }
+
 }
